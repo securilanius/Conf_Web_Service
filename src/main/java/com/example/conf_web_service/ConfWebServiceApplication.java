7ad7fd15bd27e803b2275f688423c3c7efd93c45
Iashin Iashin <securilanius@gmail.com>
2022-05-12 14:07:13 +0200

    First Commit

---
+++ src/main/java/com/example/conf_web_service/ConfWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.conf_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/conf_web_service/ConfWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conf_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
